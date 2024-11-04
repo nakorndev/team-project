89aa687c55e712889737273cb8350c6167b1f935
นาย เอ <a@mycompany.com>
2024-11-04 09:32:11 +0700

    add simple book store

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+const books = [];
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
@@ -11,6 +13,23 @@ app.get('/about', (req, res) => {
 
 app.get('/contact', (req, res) => {
   res.send('Contact page');
+});
+
+app.post('/books', (req, res) => {
+  const { title, author } = req.body;
+  const newBook = { title, author, id: books.length + 1 };
+  books.push(newBook);
+  res.send(newBook);
+});
+
+app.get('/books/:id', (req, res) => {
+  const { id } = req.params;
+  const foundBook = books.find(book => book.id === Number(id));
+  if (foundBook) {
+    res.send(foundBook);
+  } else {
+    res.status(404).send({});
+  }
 });
 
 app.listen(3000, () => {
